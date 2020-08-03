6e1a03c3cd8dc5f1be31390b6bd7f75c850492fa
Bruno Ramos <brunex0517@gmail.com>
2020-08-03 12:41:16 -0500

    Conexion, creacion de Imagen, Libros java

--- src/java/Modelo/Libros.java
+++ src/java/Modelo/Libros.java
@@ -11,4 +11,71 @@ package Modelo;
  */
 public class Libros {
     
+    private int idlibro;
+    private String nombre;
+    private String autor;
+    private String editorial;
+    private String codigo;
+    private float precio;
+    
+    public Libros(){
+        
+    }
+    
+    public Libros(String nombre, String autor, String editorial, String codigo, float precio){
+        this.nombre = nombre;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.codigo = codigo;
+        this.precio = precio;
+    }
+
+    public int getIdlibro() {
+        return idlibro;
+    }
+
+    public void setIdlibro(int idlibro) {
+        this.idlibro = idlibro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    
 }
